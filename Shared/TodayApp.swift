20eff6ed1c662ccd26f079e92e080e51144a97ac
lauryn-anderson <laurynelysse@gmail.com>
2020-09-02 22:04:49 -0600

    Initial Commit

---
+++ Shared/TodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayApp.swift
+//  Shared
+//
+//  Created by Lauryn Anderson on 2020-09-02.
+//
+
+import SwiftUI
+
+@main
+struct TodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
